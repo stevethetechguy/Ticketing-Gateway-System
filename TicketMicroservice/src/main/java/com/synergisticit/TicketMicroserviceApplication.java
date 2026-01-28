22e83762d8d9db67634769adb30d3fd3f652c8b5
Steve Burgos <steveburgos@Steves-Air.lan>
2026-01-28 10:59:11 -0600

    initialize project

---
+++ TicketMicroservice/src/main/java/com/synergisticit/TicketMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.synergisticit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMicroserviceApplication.class, args);
+	}
+
+}
